4ca1c6675bcb011cccea10b9bdd1084936007d76
joaopdmota <jp.1998.jpd@icloud.com>
2020-11-12 19:37:16 -0300

    removing coverage

--- tests/components/App.spec.js
+++
@@ -1,8 +0,0 @@
-import { mount } from '@vue/test-utils';
-import App from '../../src/App.vue';
-
-	test('displays message', () => {
-		const wrapper = mount(App);
-
-  expect(wrapper.text()).toContain('Hello world');
-});
--- tests/components/Button.spec.js
+++
@@ -1,8 +0,0 @@
-import { mount } from '@vue/test-utils';
-import Button from '../../src/components/Button/Button.vue';
-
-	test('displays message', () => {
-		const wrapper = mount(Button);
-
-  expect(wrapper.text()).toContain('Button');
-});
